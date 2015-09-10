66edfac0ac981146bf965a4c2b3cc610e5ccad44
timrae <perceptualchaos2@gmail.com>
2015-09-11 04:57:05 +0900

    Switch over to new simplified API

--- app/src/main/java/com/ichi2/apisample/MainActivity.java
+++ app/src/main/java/com/ichi2/apisample/MainActivity.java
@@ -1,16 +1,15 @@
 package com.ichi2.apisample;
 
 import android.app.Activity;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ShareCompat;
-import android.support.v7.app.AlertDialog;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.util.SparseBooleanArray;
@@ -32,10 +31,9 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
     public static final String LOG_TAG = "AnkiDroidApiSample";
-    private static final String MID = "ankidroid.mid";
-    private static final String DID = "ankidroid.did";
+    private static final int AD_PERM_REQUEST = 0;
 
     private ListView mListView;
     private ArrayList<HashMap<String, String>> mListData;
@@ -55,6 +53,16 @@ public class MainActivity extends AppCompatActivity {
         // When an item is long-pressed the ListSelectListener will make a Contextual Action Bar with Share icon
         mListView.setMultiChoiceModeListener(new ListSelectListener());
     }
+
+
+    public void onRequestPermissionsResult (int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode==AD_PERM_REQUEST && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            addCardsToAnkiDroid(getSelectedData());
+        } else {
+            Toast.makeText(MainActivity.this, R.string.permission_denied, Toast.LENGTH_LONG).show();
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -107,12 +115,12 @@ public class MainActivity extends AppCompatActivity {
                         If you don't need to share with any apps other than AnkiDroid, you can completely replace
                         this code block with the code in AnkiDroidActionProvider.onMenuItemClick()
                      **/
-                    if (!AddContentApi.isInstalled(MainActivity.this)) {
-                        // Only 1 piece of text is supported by the ACTION_SEND intent, so take first entry
-                        shareViaSendIntent(getSelectedData().get(0));
-                    } else {
+                    if (AddContentApi.getAnkiDroidPackageName(MainActivity.this) != null) {
                         // Use AnkiDroidActionProvider to handle the click event if the provider is installed
                         item.setActionProvider(new AnkiDroidActionProvider(MainActivity.this, getSelectedData()));
+                    } else {
+                        // Only 1 piece of text is supported by the ACTION_SEND intent, so take first entry
+                        shareViaSendIntent(getSelectedData().get(0));
                     }
                     return true;
                 default:
@@ -168,7 +176,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean hasSubMenu() {
             // If the AnkiDroid ContentProvider is installed then show it in a submenu, otherwise no need for submenu
-            return AddContentApi.isInstalled(MainActivity.this);
+            return AddContentApi.getAnkiDroidPackageName(MainActivity.this) != null;
         }
 
         @Override
@@ -177,10 +185,9 @@ public class MainActivity extends AppCompatActivity {
             subMenu.clear();
             PackageManager manager = getApplicationContext().getPackageManager();
             Resources res = getApplicationContext().getResources();
-
             // Add AnkiDroid "instant add" to the menu
             try {
-                ApplicationInfo appInfo = manager.getApplicationInfo(AddContentApi.getPackageName(MainActivity.this),0);
+                ApplicationInfo appInfo = manager.getApplicationInfo(AddContentApi.getAnkiDroidPackageName(MainActivity.this),0);
                 String label = manager.getApplicationLabel(appInfo) + " " + res.getString(R.string.instant_add);
                 subMenu.add(0, ANKIDROID_INSTANT_ADD, ANKIDROID_INSTANT_ADD, label)
                         .setIcon(appInfo.loadIcon(manager))
@@ -201,8 +208,15 @@ public class MainActivity extends AppCompatActivity {
         public boolean onMenuItemClick(MenuItem item) {
             // Handle when the submenu items are clicked
             if (item.getItemId() == ANKIDROID_INSTANT_ADD) {
+                // Request permission to access API if required (necessary for operation on Android 6+)
+                String reqPerm = AddContentApi.checkRequiredPermission(MainActivity.this);
+                if (reqPerm != null && ContextCompat.checkSelfPermission(MainActivity.this, reqPerm)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{reqPerm}, AD_PERM_REQUEST);
+                    return true;
+                }
                 // Add all data using AnkiDroid provider
-                configureAndSendToAnkiDroid(getSelectedData());
+                addCardsToAnkiDroid(mSelectedData);
             } else if (item.getItemId() == ALL_APPS) {
                 // If the user presses "more" then switch to the stock Android intent selector (can only send 1 card)
                 shareViaSendIntent(mSelectedData.get(0));
@@ -228,87 +242,34 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void configureAndSendToAnkiDroid(final ArrayList<HashMap<String, String>> data) {
+    /**
+     * Use the instant-add API to add flashcards directly to AnkiDroid
+     * @param data List of cards to be added. Each card has a HashMap of field name / field value pairs.
+     */
+    private void addCardsToAnkiDroid(final ArrayList<HashMap<String, String>> data) {
         // Get api instance
         final AddContentApi api = new AddContentApi(MainActivity.this);
-        // Check if a valid model ID and deck ID have been stored in preferences
-        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
-        Long mid = prefs.getLong(MID, -1);
-        Long did = prefs.getLong(DID, -1);
-        // Try to find by name if they weren't stored in preferences (will work if the user hasn't changed the names)
-        if (mid == -1) {
-            mid = api.findModelId(AnkiDroidConfig.MODEL_NAME, AnkiDroidConfig.FIELDS.length);
+        // Look for our deck, add a new one if it doesn't exist
+        Long did = api.findDeckIdByName(this, AnkiDroidConfig.DECK_NAME);
+        if (did == null) {
+            did = api.addNewDeck(this, AnkiDroidConfig.DECK_NAME);
         }
-        if (did == -1) {
-            did = api.getDeckId(AnkiDroidConfig.DECK_NAME);
+        // Look for our model, add a new one if it doesn't exist
+        Long mid = api.findModelIdByName(this, AnkiDroidConfig.MODEL_NAME, AnkiDroidConfig.FIELDS.length);
+        if (mid == null) {
+            mid = api.addNewCustomModel(this, AnkiDroidConfig.MODEL_NAME, AnkiDroidConfig.FIELDS,
+                    AnkiDroidConfig.CARD_NAMES, AnkiDroidConfig.QFMT, AnkiDroidConfig.AFMT,
+                    AnkiDroidConfig.CSS, did);
         }
-        final boolean addModel = api.getModelName(mid) == null;
-        final boolean addDeck = api.getDeckName(did) == null;
-
-        if (addModel || addDeck) {
-            // If a valid mid and did could not be found then add a new model and deck to AnkiDroid
-            AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.Theme_AppCompat_Dialog);
-            builder.setMessage(R.string.confirm_add_models);
-            builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    Long did = null;
-                    if (addDeck) {
-                        try {
-                            did = api.addNewDeck(AnkiDroidConfig.DECK_NAME);
-                            prefs.edit().putLong(DID, did).commit();
-                        } catch (Exception e) {
-                            Log.e(LOG_TAG, "Error adding deck "+AnkiDroidConfig.DECK_NAME+" to AnkiDroid via API", e);
-                            Toast.makeText(MainActivity.this, R.string.deck_add_fail, Toast.LENGTH_LONG).show();
-                            return;
-                        }
-                    }
-                    if (addModel) {
-                        try {
-                            Long mid = api.addNewModel(AnkiDroidConfig.MODEL_NAME, AnkiDroidConfig.FIELDS,
-                                    AnkiDroidConfig.CARD_NAMES, AnkiDroidConfig.QFMT, AnkiDroidConfig.AFMT,
-                                    AnkiDroidConfig.CSS, did);
-                            prefs.edit().putLong(MID, mid).commit();
-                        } catch (Exception e) {
-                            Log.e(LOG_TAG, "Error adding model " + AnkiDroidConfig.MODEL_NAME + " to AnkiDroid via API", e);
-                            Toast.makeText(MainActivity.this, R.string.model_add_fail, Toast.LENGTH_LONG).show();
-                            return;
-                        }
-                    }
-                    addCardsToAnkiDroid(data);
-                }
-            });
-            builder.setNegativeButton(android.R.string.cancel, null);
-            builder.show();
-        } else {
-            // No need to add
-            addCardsToAnkiDroid(data);
-        }
-    }
-
-    /**
-     * Add flashcards to AnkiDroid. A valid model ID must be given or an exception will be thrown.
-     * Call configureAndSendToAnkiDroid() first to ensure that the model and deck are setup properly.
-     * @param data
-     */
-    private void addCardsToAnkiDroid(ArrayList<HashMap<String, String>> data) {
-        Resources res = getResources();
-        // Get api instance
-        AddContentApi api = new AddContentApi(MainActivity.this);
-        // Get mid and did from prefs
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
-        Long mid = prefs.getLong(MID, -1);
-        Long did = prefs.getLong(DID, -1);
-        if (mid == -1 || did == -1) {
-            throw new IllegalArgumentException("mid and did not saved in shared preferences");
+        // Double-check that everything was added correctly
+        String[] fieldNames = api.getFieldList(mid);
+        if (mid == null || did == null || fieldNames == null) {
+            Toast.makeText(MainActivity.this, R.string.card_add_fail, Toast.LENGTH_LONG).show();
+            return;
         }
         // Add cards
         int added = 0;
         for (HashMap<String, String> hm: data) {
-            String[] fieldNames = api.getFieldList(mid);
-            if (fieldNames == null) {
-                throw new IllegalArgumentException("Specified model doesn't exist");
-            }
             // Build a field map accounting for the fact that the user could have changed the fields in the model
             String[] flds = new String[fieldNames.length];
             for (int i = 0; i < flds.length; i++) {
@@ -317,15 +278,18 @@ public class MainActivity extends AppCompatActivity {
                     flds[i] = hm.get(AnkiDroidConfig.FIELDS[i]);
                 }
             }
+            // Add a new note using the current field map
             try {
-                api.addNewNote(mid, did, flds, AnkiDroidConfig.TAGS);
+                Uri noteUri = api.addNewNote(mid, did, flds, AnkiDroidConfig.TAGS);
+                if (noteUri != null) {
+                    added++;
+                }
             } catch (Exception e) {
                 Log.e(LOG_TAG, "Exception adding cards to AnkiDroid", e);
                 Toast.makeText(MainActivity.this, R.string.card_add_fail, Toast.LENGTH_LONG).show();
                 return;
             }
-            added++;
         }
-        Toast.makeText(MainActivity.this, res.getString(R.string.n_items_added, added), Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this, getResources().getString(R.string.n_items_added, added), Toast.LENGTH_LONG).show();
     }
 }
