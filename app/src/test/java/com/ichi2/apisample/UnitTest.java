b0d5682348b11fb8444a4c0fce38d5fa65f3f8c4
Eugene Melnik <e.melnik@luckywarepro.com>
2020-09-18 18:16:37 +0300

    Base tests

---
+++ app/src/test/java/com/ichi2/apisample/UnitTest.java
@@ -0,0 +1,22 @@
+package com.ichi2.apisample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class UnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void isTrue() {
+        assertTrue(true);
+    }
+}
