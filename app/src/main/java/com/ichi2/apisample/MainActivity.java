80371a1378078dedc27181275384b1e870523a65
Eugene Melnik <e.melnik@luckywarepro.com>
2020-10-02 17:41:44 +0300

    Increase target sdk version

--- app/src/main/java/com/ichi2/apisample/MainActivity.java
+++ app/src/main/java/com/ichi2/apisample/MainActivity.java
@@ -16,6 +16,7 @@ import android.widget.Toast;
 // @todo Radiobuttons for direction/timing instead of input field
 // @todo Save last entered values on close and restore next time
 // @todo Save instruments, tempos ... and supply in input field
+// @todo Implement "select file" function (see feature/select-file-dialog branch)
 
 public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
 
