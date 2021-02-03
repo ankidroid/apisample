d3ab7a7b9de0c4af05aae060ca22d7fe5150f34e
Yaroslav Gavrilenko <y.gavrilenko@luckywarepro.com>
2021-02-03 16:15:00 +0200

    Minor change

--- app/src/main/java/com/ichi2/apisample/MainActivity.java
+++ app/src/main/java/com/ichi2/apisample/MainActivity.java
@@ -376,7 +376,7 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
             if (isExternalStorageDocument(uri)) {
                 final String docId = DocumentsContract.getDocumentId(uri);
                 final String[] split = docId.split(":");
-                return "/storage/" + String.join("/", split);
+                return "/storage/" + split[0] + "/" + split[1];
             } else if (isDownloadsDocument(uri)) {
                 final String id = DocumentsContract.getDocumentId(uri);
                 uri = ContentUris.withAppendedId(
