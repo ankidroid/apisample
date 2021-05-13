53ee75603734ab6d5d2c390f6e8e019e32883b95
Yaroslav Gavrilenko <y.gavrilenko@luckywarepro.com>
2021-05-13 12:15:50 +0300

    Create StringUtil

---
+++ app/src/main/java/com/ichi2/apisample/helper/StringUtil.java
@@ -0,0 +1,18 @@
+package com.ichi2.apisample.helper;
+
+public class StringUtil {
+    public static String joinStrings(String separator, String[] values) {
+        StringBuilder builder = new StringBuilder();
+        for (String value : values) {
+            if (builder.length() > 0) {
+                builder.append(separator);
+            }
+            builder.append(value);
+        }
+        return builder.toString();
+    }
+
+    public static String[] splitStrings(String separator, String str) {
+        return !str.isEmpty() ? str.split(separator) : new String[]{};
+    }
+}
